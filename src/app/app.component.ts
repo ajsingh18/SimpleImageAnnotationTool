e25ce91b679ec24b93b2a184669e17d7c8777247
ajsingh18 <47607807+ajsingh18@users.noreply.github.com>
2020-02-22 15:09:34 -0700

    Image upload
    Initial push. Image upload working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { faLaughBeam } from '@fortawesome/free-solid-svg-icons';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'test';
+  LaughBeam = faLaughBeam;
+  Frown = faFrown;
+  public imagePath;
+  url: any;
+  dragPosition = {x: 0, y: 0};
+
+public displayImage(event: any) {
+
+  if (event.target.files && event.target.files[0]) {
+          var reader = new FileReader();
+          reader.onload = (event: any) => {
+              this.url = event.target.result;
+          }
+          reader.readAsDataURL(event.target.files[0]);
+      }
 }
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    FontAwesomeModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
